8489ba31989b679e10173596f3e76cbc85c23f04
suncream100 <murmuring91@gmail.com>
2024-08-12 19:03:53 +0900

    add: 5주차 - effect 1

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,14 @@
-import Button from "./Button";
+import { useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log(counter);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text="클릭" />
+      <h1 className={styles.title}>Welcome back!!! {counter}</h1>
+      <button onClick={onClick}>클릭</button>
     </div>
   );
 }
