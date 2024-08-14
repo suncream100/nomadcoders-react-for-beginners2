97020883a799494b759871edbaab29e339f9aec1
suncream100 <murmuring91@gmail.com>
2024-08-14 10:13:49 +0900

    update: 5주차 - effect3

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,38 +1,26 @@
 import { useState, useEffect } from "react";
-import styles from "./App.module.css";
-
+function Hello() {
+  // function byeFn() {
+  //   console.log("destroyed :(");
+  // }
+  // function hiFn() {
+  //   console.log("created :)");
+  //   return byeFn;
+  // }
+  // useEffect(hiFn, [])
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  },[])
+  return <h1>Hello</h1>
+}
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("deps가 비어있으면 한번만 실행");
-  }, []);
-  useEffect(() => {
-    console.log("keyword값이 변경될 때마다 실행");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("counter값이 변경될 때마다 실행");
-  }, [counter]);
-  useEffect(() => {
-    console.log("keyword와 counter값이 변경될 때마다 실행");
-  }, [keyword, counter]);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("call the api...", keyword);
-    }
-  }, [keyword]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search here..."
-        value={keyword}
-        onChange={onChange}
-      />
-      <h1 className={styles.title}>Welcome back!!! {counter}</h1>
-      <button onClick={onClick}>클릭</button>
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
