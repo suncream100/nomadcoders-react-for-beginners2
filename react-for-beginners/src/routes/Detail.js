4c3be7e7b36a38ff0976a2fa53d92dc69e85056c
suncream100 <murmuring91@gmail.com>
2024-09-03 17:34:26 +0900

    update: movie app 개선 - 상세페이지 loading 위치 수정

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -57,81 +57,84 @@ function Detail() {
   },[]);
   return (
     <Fragment>
-      {!loading ? <div className="wrap_movie wrap_detail">
+      <div className="wrap_movie wrap_detail">
         <h1 className="tit_movie">개봉예정작 ✨</h1>
-        <div className="box_g">
-          <div className={styles.summary_detail}>
-            <div className="wrap_thumb">
-              {!movie.poster_path ? <span className="ico_noimg"></span> : <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="poster" />}
-            </div>
-            <dl className={styles.info_movie}>
-              <div>
-                <dt>제목</dt>
-                <dd>{movie.title}{movie.title !== movie.original_title ? ` (${movie.original_title})` : null}</dd>
+        {!loading ? 
+          <div className="box_g">
+            <div className={styles.summary_detail}>
+              <div className="wrap_thumb">
+                {!movie.poster_path ? <span className="ico_noimg"></span> : <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="poster" />}
               </div>
-              <div>
-                <dt>개봉예정일</dt>
-                <dd>{movie.release_date}</dd>
-              </div>
-              <div>
-                <dt>상영시간</dt>
-                <dd>{movie.runtime ? movie.runtime : '-'}분</dd>
-              </div>
-              <div>
-                <dt>장르</dt>
-                <dd>
-                  {movie.genres ?
-                    movie.genres.map((item) => (
-                      <span className={styles.txt_info} key={item.id}>{item.name}</span>
-                    ))
-                    : '-'
-                  }
-                </dd>
-              </div>
-              <div>
-                <dt>국가</dt>
-                <dd>
-                  {movie.production_countries ?
-                    movie.production_countries.map((item) => {
-                      const country = countryName.find(obj => obj.iso_3166_1 === item.iso_3166_1);
-                      return (
-                        <span className={styles.txt_info} key={item.iso_3166_1}>
-                          {country ? country.native_name : '-'}
-                        </span>
-                      );
-                    }) 
-                    : '-'
-                  }
-                </dd>
-              </div>
-              <div>
-                <dt>소개</dt>
-                <dd>{movie.overview}</dd>
-              </div>
-            </dl>
-          </div>
-          <div className={styles.credit_detail}>
-            <h2 className={styles.tit_credit}>감독/출연</h2>
-            <ul className={styles.list_credit}>
-              {credit.map((item) => (
-                <li key={item.credit_id}>
-                  <div className="wrap_thumb">
-                    {!item.profile_path ? <span className="ico_noimg"></span> : <img src={`https://image.tmdb.org/t/p/w200${item.profile_path}`} alt="profile" />}
-                  </div>
-                  <div className={styles.cont_credit}>
-                    <h2 className={styles.tit_profile}>{item.name}</h2>
-                    {item.department !== 'Directing' && item.character === '' ? null :
-                      <span className={styles.txt_job}>
-                        {item.department === 'Directing' ? '감독' : `${item.character === 'Self' ? '본인' : item.character} 역`}
-                      </span>
+              <dl className={styles.info_movie}>
+                <div>
+                  <dt>제목</dt>
+                  <dd>{movie.title}{movie.title !== movie.original_title ? ` (${movie.original_title})` : null}</dd>
+                </div>
+                <div>
+                  <dt>개봉예정일</dt>
+                  <dd>{movie.release_date}</dd>
+                </div>
+                <div>
+                  <dt>상영시간</dt>
+                  <dd>{movie.runtime ? movie.runtime : '-'}분</dd>
+                </div>
+                <div>
+                  <dt>장르</dt>
+                  <dd>
+                    {movie.genres ?
+                      movie.genres.map((item) => (
+                        <span className={styles.txt_info} key={item.id}>{item.name}</span>
+                      ))
+                      : '-'
                     }
-                  </div>
-                </li>
-              ))}
-            </ul>
+                  </dd>
+                </div>
+                <div>
+                  <dt>국가</dt>
+                  <dd>
+                    {movie.production_countries ?
+                      movie.production_countries.map((item) => {
+                        const country = countryName.find(obj => obj.iso_3166_1 === item.iso_3166_1);
+                        return (
+                          <span className={styles.txt_info} key={item.iso_3166_1}>
+                            {country ? country.native_name : '-'}
+                          </span>
+                        );
+                      }) 
+                      : '-'
+                    }
+                  </dd>
+                </div>
+                <div>
+                  <dt>소개</dt>
+                  <dd>{movie.overview}</dd>
+                </div>
+              </dl>
+            </div>
+            <div className={styles.credit_detail}>
+              <h2 className={styles.tit_credit}>감독/출연</h2>
+              <ul className={styles.list_credit}>
+                {credit.map((item) => (
+                  <li key={item.credit_id}>
+                    <div className="wrap_thumb">
+                      {!item.profile_path ? <span className="ico_noimg"></span> : <img src={`https://image.tmdb.org/t/p/w200${item.profile_path}`} alt="profile" />}
+                    </div>
+                    <div className={styles.cont_credit}>
+                      <h2 className={styles.tit_profile}>{item.name}</h2>
+                      {item.department !== 'Directing' && item.character === '' ? null :
+                        <span className={styles.txt_job}>
+                          {item.department === 'Directing' ? '감독' : `${item.character === 'Self' ? '본인' : item.character} 역`}
+                        </span>
+                      }
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-      </div> : null}
+          : null
+        }
+      </div>
       {loading ? <Loading /> : null}
     </Fragment>
   )
